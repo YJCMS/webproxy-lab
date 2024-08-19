51637aa07e386e8d2079edca9f5b0c021d87870e
YJCMS <sen9563@gmail.com>
2024-08-19 05:10:46 +0000

    add nefpfragments

--- netp/netpfragments.h
+++ netp/netpfragments.h
@@ -7,26 +7,26 @@ struct in_addr {
 };
 
 /* IP socket address structure */
-strict sockaddr_in {
-    uint16_t    sin_family; /* Protocol family(always AF_INET) */
-    uint16_t    sin_prot; /* Port number in network byte order */
-    struct in_addr  sin_addr; /* IP address in network byte order */
-    unsigned char   sin_zero[8] /* Pad to sizeof(struct sockaddr) */
+struct sockaddr_in {
+    uint16_t            sin_family; /* Protocol family(always AF_INET) */
+    uint16_t            sin_port; /* Port number in network byte order */
+    struct in_addr      sin_addr; /* IP address in network byte order */
+    unsigned char       sin_zero[8]; /* Pad to sizeof(struct sockaddr) */
 };
 
 /* Generic socket address structure (for connect, bind, and accept) */
 struct  sockaddr {
-    uint16_t sa_family_t; /* Protocol family */
-    char    sa_data[14]; /* Address data */
+    uint16_t    sa_family_t; /* Protocol family */
+    char        sa_data[14]; /* Address data */
 };
 
 struct addrinfo {
-    int ai_flags;   /* Hints argument flags */
-    int ai_family; /* First arg to socket function */
-    int ai_socktype; /* Second arg to socket function */
-    int ai_protocol; /* Third arg to socket function */
-    char *int_canonname; /* Canonical hostname */
-    size_t ai_addrlen; /* Ptr to socket address structure */
+    int             ai_flags;   /* Hints argument flags */
+    int             ai_family; /* First arg to socket function */
+    int             ai_socktype; /* Second arg to socket function */
+    int             ai_protocol; /* Third arg to socket function */
+    char            *int_canonname; /* Canonical hostname */
+    size_t          ai_addrlen; /* Ptr to socket address structure */
     struct sockaddr *ai_addr; /* Ptr to socket address structure */
     struct sockaddr *ai_next; /* Ptr to next item in linked list */
 };
